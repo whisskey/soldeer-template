c46fdf5423aa819600f6dac97e34fb363e6bab83
Can Vardar <ismailcanvardar@gmail.com>
2024-07-16 22:18:00 +0300

    refactor: delete Counter.sol
    - Counter.sol and its related contracts are deleted.
    - Foo.sol and its related contracts are created for the place of Counter.sol.

---
+++ src/Foo.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21 <0.9.0;
+
+contract Foo {
+    function bar() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
---
+++ test/Foo.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Foo} from "../src/Foo.sol";
+
+contract FooTest is Test {
+    Foo public foo;
+
+    function setUp() public {
+        foo = new Foo();
+    }
+
+    function test_Bar() public view {
+        string memory result = foo.bar();
+        string memory expected = "Hello, World!";
+        assertEq(
+            keccak256(abi.encodePacked(result)),
+            keccak256(abi.encodePacked(expected))
+        );
+    }
+}
