241b3195a3a843550e63d880506342d2e0359700
Whiskey <Berkoic@hotmail.com>
2024-10-25 17:01:24 +0300

    chore: update version

--- src/Foo.sol
+++ src/Foo.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.26;
+pragma solidity >=0.8.27;
 
 contract Foo {
     function bar() external pure returns (string memory) {
--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.26;
+pragma solidity >=0.8.27;
 
 import { Foo } from "../src/Foo.sol";
-import { Test } from "forge-std/src/Test.sol";
+import { Test } from "forge-std/Test.sol";
 
 contract FooTest is Test {
     Foo public foo;
